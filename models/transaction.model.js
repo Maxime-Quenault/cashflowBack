7d62a0fdbc36e1c4d5ff5286587596084e8d90bc
Maxime-Quenault <77842815+Maxime-Quenault@users.noreply.github.com>
2023-03-25 12:21:58 +0100

    ajout model transaction

---
+++ models/transaction.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema(
+    {
+        value: {type: Number, required: true},
+        title: {type: String, required: true, trim: true},
+        category: {type: String, required: true, trim: true},
+        dateOfTransaction: {type: String, required: true, trim: true},
+        _idProfile: {type: String, required: true, trim: true}
+    },
+    {
+        collection: 'transactions',
+        versionKey: false
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default mongoose.model("transactions", transactionSchema);
